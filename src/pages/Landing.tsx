7faeb6cf943c318ff85a89174369f0a37b2cf094
roskzhu <roskenzhu@gmail.com>
2023-11-05 01:24:49 -0500

    added home text and gradient backgrounds

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -8,12 +8,15 @@ import Projects from '../components/Projects';
 const Landing: React.FC = () => {
   return (
     <LandingContainer>
-      {/* fix */}
-      {/* <GradientCircle/>  */}
-      <Home />
+      <GradientCircle style={{ marginTop: '3%', background: '#BBC6FF'}}/>
+      <GradientCircle style={{ marginTop: '7%',  marginLeft: '20%', background: '#FFBBF0'}}/>
+      <GradientCircle style={{ marginTop: '-3%',  marginLeft: '45%'}}/>      
+      <Home/>    
+      <GradientCircle style={{ width: '10%', height: '10%', marginTop: '40%', marginLeft: '790px', background: '#FFB69E'}}/>        
       <About/>
       <Experience />
       <Projects />
+      <GradientCircle style={{ width: '40%', height: '5%', marginTop: '190%', marginLeft: '25%', background: '#9EB4FF',  filter: 'blur(300px)'}}/>        
     </LandingContainer>
   );
 };
@@ -21,11 +24,13 @@ const Landing: React.FC = () => {
 const LandingContainer = styled.div`
   display: flex;
   flex-direction: column;
+  overflow: hidden;
+  position: relative;
 `;
 
 const GradientCircle = styled.div`
-  width: 310.98px;
-  height: 316.616px;
+  width: 15%;
+  height: 15%;
   transform: rotate(-0.167deg);
   flex-shrink: 0;
   border-radius: 316.616px;
@@ -33,27 +38,13 @@ const GradientCircle = styled.div`
   background: #FFE977;
   filter: blur(225px);
   margin-top: 25%;
-  margin-left: 75%;
-  margin-right: 10%;
+  margin-left: 0%;
+  margin-right: 0%;
+  position: absolute;
   z-index: -1;
-  animation: pulsate 5s infinite; /* Add an animation with a name of 'pulsate', 2-second duration, and it will repeat infinitely */
-  
-  @keyframes pulsate {
-    0% {
-      transform: scale(0.95); /* Set the initial scale of the circle */
-      filter: blur(225px); /* Apply the blur effect */
-    }
-    50% {
-      transform: scale(1); /* Set the scale to 1 in the middle of the animation */
-      filter: blur(150px); /* Decrease the blur effect */
-      opacity: 0%;
-    }
-    100% {
-      transform: scale(0.95); /* Return to the initial scale at the end of the animation */
-      filter: blur(225px); /* Apply the blur effect again */
-    }
-  }
+
 `;
+
 
 
 export default Landing;
