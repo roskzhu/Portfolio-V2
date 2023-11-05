1c78f3f75a84825f87fa1a342cb11df1f0a4e342
roskzhu <roskenzhu@gmail.com>
2023-11-05 02:28:14 -0500

    added gradient animations

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -8,15 +8,15 @@ import Projects from '../components/Projects';
 const Landing: React.FC = () => {
   return (
     <LandingContainer>
-      <GradientCircle style={{ marginTop: '3%', background: '#BBC6FF'}}/>
-      <GradientCircle style={{ marginTop: '7%',  marginLeft: '20%', background: '#FFBBF0'}}/>
-      <GradientCircle style={{ marginTop: '-6%',  marginLeft: '45%'}}/>      
+      <GradientCircle style={{ marginTop: '3%', background: '#BBC6FF', animationDelay: '1s' }} />
+      <GradientCircle style={{ marginTop: '7%', marginLeft: '20%', background: '#FFBBF0', animationDelay: '0.75s' }} />
+      <GradientCircle style={{ marginTop: '-6%', marginLeft: '45%', animationDelay: '0.5s' }} />
       <Home/>    
-      <GradientCircle style={{ width: '10%', height: '10%', marginTop: '40%', marginLeft: '790px', background: '#FFB69E'}}/>        
+      <GradientCircle style={{ width: '10%', height: '10%', marginTop: '40%', marginLeft: '790px', background: '#FFB69E', animationDelay: '0.25s' }}/>        
       <About/>
       <Experience />
       <Projects />
-      <GradientCircle style={{ width: '40%', height: '5%', marginTop: '190%', marginLeft: '25%', background: '#9EB4FF',  filter: 'blur(300px)'}}/>        
+      <GradientCircle style={{ width: '40%', height: '5%', marginTop: '190%', marginLeft: '25%', background: '#9EB4FF',  filter: 'blur(300px)', animationDelay: '1.25s' }}/>        
     </LandingContainer>
   );
 };
@@ -42,7 +42,23 @@ const GradientCircle = styled.div`
   margin-right: 0%;
   position: absolute;
   z-index: -1;
-
+  animation: pulsate 8s infinite; /* Add an animation with a name of 'pulsate', 2-second duration, and it will repeat infinitely */
+  
+  @keyframes pulsate {
+    0% {
+      transform: scale(0.95); /* Set the initial scale of the circle */
+      filter: blur(225px); /* Apply the blur effect */
+    }
+    50% {
+      transform: scale(1); /* Set the scale to 1 in the middle of the animation */
+      filter: blur(150px); /* Decrease the blur effect */
+      opacity: 80%;
+    }
+    100% {
+      transform: scale(0.95); /* Return to the initial scale at the end of the animation */
+      filter: blur(225px); /* Apply the blur effect again */
+    }
+  }
 `;
 
 
