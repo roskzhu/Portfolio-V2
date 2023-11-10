9de257e541f700a17bba66ee226e9fa3fad2d555
roskzhu <roskenzhu@gmail.com>
2023-11-10 01:51:07 -0500

    fade timing fix

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -46,7 +46,7 @@ class About extends React.Component<{}, AboutState> {
 
     return (
       <div id="about" className="about-container">
-        <FadeIn delay={`1200ms`}>
+        <FadeIn delay={`200ms`}>
           <div className="section-header">
             <span className="section-title">./ about</span>
           </div>
