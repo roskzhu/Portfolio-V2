5cdb03a81bc45ca733104388e598f1f9e3ea9e02
roskzhu <roskenzhu@gmail.com>
2023-11-13 15:53:05 -0500

    fix dep

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -28,7 +28,7 @@ class Experience extends React.Component<{}, ExperienceState> {
         <div className="experience-container">
         <FadeIn>
           <div className="section-header" style={{ paddingBottom: '20px'}}>
-            <span className="section-title-before">//  Real impact made for real people.</span>
+            <span className="section-title-before">{"// "}Real impact made for real people.</span>
             <span className="section-title">Experience</span>
           </div>
           <Work></Work>
